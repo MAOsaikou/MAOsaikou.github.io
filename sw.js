2154009c5740cf76743330ef9c6e85589ef4ae11
MAOsaikou <2639083951@qq.com>
2025-08-27 17:00:17 +0800

    Site updated: 2025-08-27 17:00:15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756284945681";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756285211035";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
